6ca61ed47fbeba8108831274fe8b37498af3decc
Eliel Carvalho <elielrty@gmail.com>
2020-03-16 19:25:02 -0300

    estudos

--- Express/index.js
+++ Express/index.js
@@ -1,5 +1,30 @@
 const express = require('express')
 const app = express()
 
+app.use('/opa', (req, res, next) => {
+    console.log('Antes...')
+    next()
+})
 
-app.listen(3000)
+app.use('/opa',(req, res, next) =>{
+    console.log('Durante...')
+    res.json([
+        {id: 7, name: 'Ana', position: 1},
+        {id: 10, name: 'calos', position: 2},
+        {id: 15, name: 'daniel', position: 3}
+    ])
+    next()
+    //res.send('<h1>Estou bem!</h1>') // pode retornar html tb 
+    // res.json({
+    //     name: 'ipad 32GB',
+    //     price:1899.00,
+    //     discont: 0.12
+    // })
+})
+
+app.use('/opa', (req, res) => {
+    console.log('Depois...')
+})
+app.listen(3001, () => {
+    console.log('Backend executando...')
+})
